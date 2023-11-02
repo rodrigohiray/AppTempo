735b0fffb5dff9749dd7da350db30a5b9bde3dd6
Rodrigo Passos <92394086+rodrigohiray@users.noreply.github.com>
2023-11-02 19:45:28 +0000

    Criação Data e Nome

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -24,9 +26,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -74,7 +83,9 @@ fun Cabecalho(): Unit {
             onClick = { /*TODO*/ },
             colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
             contentPadding = PaddingValues(),
-            modifier = Modifier.size(35.dp).clip(CircleShape)
+            modifier = Modifier
+                .size(35.dp)
+                .clip(CircleShape)
 
 
             ) {
@@ -87,7 +98,9 @@ fun Cabecalho(): Unit {
             onClick = { /*TODO*/ },
             colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
             contentPadding = PaddingValues(),
-            modifier = Modifier.size(40.dp).clip(CircleShape)
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
 
         ) {
 
@@ -107,6 +120,45 @@ fun Cabecalho(): Unit {
     }
 
 
+
+
+
+@Preview(showBackground = true, widthDp = 380)
+@Composable
+fun dataNome(nome:String="Rodrigo"): Unit {
+    Column (modifier = Modifier.fillMaxWidth()){
+        Row (modifier = Modifier.fillMaxWidth()){
+            Text(text = "Olá",
+                modifier = Modifier.padding(end = 2.dp),
+                fontSize = TextUnit(25f, TextUnitType.Sp)
+            )
+            Text(text = "$nome",
+                fontWeight = FontWeight.Bold,
+                fontSize = TextUnit(25f, TextUnitType.Sp),
+                color = MaterialTheme.colorScheme.primary
+
+            )
+        }
+            Text(text = formatData(Calendar.getInstance().time),
+                fontWeight = FontWeight.Light,
+                fontSize = TextUnit(18f, TextUnitType.Sp),
+                color = MaterialTheme.colorScheme.secondary
+
+
+            )
+            Text(text = "Póvoa de Santa Iria",
+                fontWeight = FontWeight.Light,
+                fontSize = TextUnit(18f, TextUnitType.Sp),
+                color = MaterialTheme.colorScheme.secondary
+            )
+    }
+}
+
+
+fun formatData(data: Date): String {
+    val format = SimpleDateFormat("dd MMMM, EEEE yyyy", Locale("pt", "PT"))
+    return format.format(data)
+}
 
 
 
